003841bdd398b16238174a9133bde4c69bd12286
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2023-08-22 13:26:01 +0200

    Modelo version 1.0

---
+++ src/main/java/com/afdm/gestionpedidos/model/Product.java
@@ -0,0 +1,144 @@
+package com.afdm.gestionpedidos.model;
+
+import java.util.Objects;
+
+public class Product {
+	private Category category;
+	private int productID;
+	private String productName;
+	private int quantityPerUnit;
+	private double unitPrice;
+	private int unitsIntStock;
+	private int unitsOnOrder;
+	private String reorderLevel;
+	private boolean discontinued;
+	
+	public Product() {}
+
+	public Product(Category category, int productID, String productName, int quantityPerUnit, double unitPrice,
+			int unitsIntStock, int unitsOnOrder, String reorderLevel, boolean discontinued) {
+		
+		this.category = category;
+		this.productID = productID;
+		this.productName = productName;
+		this.quantityPerUnit = quantityPerUnit;
+		this.unitPrice = unitPrice;
+		this.unitsIntStock = unitsIntStock;
+		this.unitsOnOrder = unitsOnOrder;
+		this.reorderLevel = reorderLevel;
+		this.discontinued = discontinued;
+	}
+	public Product(Category category,String productName, int quantityPerUnit, double unitPrice,
+			int unitsIntStock, int unitsOnOrder, String reorderLevel, boolean discontinued) {
+		
+		this.category = category;
+		this.productName = productName;
+		this.quantityPerUnit = quantityPerUnit;
+		this.unitPrice = unitPrice;
+		this.unitsIntStock = unitsIntStock;
+		this.unitsOnOrder = unitsOnOrder;
+		this.reorderLevel = reorderLevel;
+		this.discontinued = discontinued;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public int getProductID() {
+		return productID;
+	}
+
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public int getQuantityPerUnit() {
+		return quantityPerUnit;
+	}
+
+	public void setQuantityPerUnit(int quantityPerUnit) {
+		this.quantityPerUnit = quantityPerUnit;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public int getUnitsIntStock() {
+		return unitsIntStock;
+	}
+
+	public void setUnitsIntStock(int unitsIntStock) {
+		this.unitsIntStock = unitsIntStock;
+	}
+
+	public int getUnitsOnOrder() {
+		return unitsOnOrder;
+	}
+
+	public void setUnitsOnOrder(int unitsOnOrder) {
+		this.unitsOnOrder = unitsOnOrder;
+	}
+
+	public String getReorderLevel() {
+		return reorderLevel;
+	}
+
+	public void setReorderLevel(String reorderLevel) {
+		this.reorderLevel = reorderLevel;
+	}
+
+	public boolean isDiscontinued() {
+		return discontinued;
+	}
+
+	public void setDiscontinued(boolean discontinued) {
+		this.discontinued = discontinued;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(productID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean iguales = false;
+		if(obj != null && obj instanceof Customer) {
+			Product otro = (Product)obj;
+			iguales = this.productID == otro.productID;
+		}
+		return iguales;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Product [category=").append(category.getCategoryName()).append(", productID=").append(productID)
+				.append(", productName=").append(productName).append(", quantityPerUnit=").append(quantityPerUnit)
+				.append(", unitPrice=").append(unitPrice).append(", unitsIntStock=").append(unitsIntStock)
+				.append(", unitsOnOrder=").append(unitsOnOrder).append(", reorderLevel=").append(reorderLevel)
+				.append(", discontinued=").append(discontinued).append("]");
+		return builder.toString();
+	}
+	
+	
+	
+}
