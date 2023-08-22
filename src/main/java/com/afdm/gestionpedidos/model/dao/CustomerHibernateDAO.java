e502b34bf6a2c77ddc75684e8990d7626505859d
elSuda_K <ghjankowiec@gmail.com>
2023-08-22 13:41:03 +0200

    CustomerHibernateDAO v1.0

---
+++ src/main/java/com/afdm/gestionpedidos/model/dao/CustomerHibernateDAO.java
@@ -0,0 +1,73 @@
+package com.afdm.gestionpedidos.model.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.afdm.gestionpedidos.model.Customer;
+
+import jakarta.persistence.TypedQuery;
+
+public class CustomerHibernateDAO implements CustomerDAO{
+
+	@Override
+	public Customer findCustomerById(String customerId) {
+		Customer customer = null;
+		Configuration conf = new Configuration();
+		conf.configure();//leyendo el fichero hibernate.cfg.xml
+		Session session = DaoUtility.getSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			customer = session.get(Customer.class, customerId);
+			transaction.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			transaction.rollback();
+		}
+		session.close();
+		return customer;
+	}
+
+	@Override
+	public List<Customer> findCustomerByCompanyName(String companyName) {
+		List<Customer> customersByCompanyNameList = new ArrayList<>();
+		Configuration conf = new Configuration();
+		conf.configure();//leyendo el fichero hibernate.cfg.xml
+		Session session = DaoUtility.getSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			TypedQuery<Customer> sentence = session.createQuery("select c from customer c where companyName like :name ", Customer.class);
+			sentence.setParameter("name", "%" + companyName + "%");
+			customersByCompanyNameList = sentence.getResultList();
+			transaction.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			transaction.rollback();
+		}
+		session.close();
+		return customersByCompanyNameList;
+	}
+
+	@Override
+	public List<Customer> findAllCustomer() {
+		List<Customer> customersList = new ArrayList<>();
+		Configuration conf = new Configuration();
+		conf.configure();//leyendo el fichero hibernate.cfg.xml
+		Session session = DaoUtility.getSession();
+		Transaction transaction = session.beginTransaction();
+		try {
+			TypedQuery<Customer> sentence = session.createQuery("select c from customer c", Customer.class);
+			customersList = sentence.getResultList();
+			transaction.commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			transaction.rollback();
+		}
+		session.close();
+		return customersList;
+	}
+}
