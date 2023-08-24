8f05d3f6048aa46c6e08366262a5ac1db6e8c23a
elSuda_K <ghjankowiec@gmail.com>
2023-08-24 13:42:16 +0200

    modificado CerrarSesionServlet y web.xml agregado CerrarSesionServlet

--- src/main/java/com/afdm/gestionpedidos/controller/CerrarSesionServlet.java
+++ src/main/java/com/afdm/gestionpedidos/controller/CerrarSesionServlet.java
@@ -3,13 +3,16 @@ package com.afdm.gestionpedidos.controller;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class CerrarSesionServlet {
+public class CerrarSesionServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			request.getSession().invalidate();
-			response.sendRedirect("gestionPedidos.jsp");
+			response.sendRedirect("inicioSesion.jsp");
 		}
 }
