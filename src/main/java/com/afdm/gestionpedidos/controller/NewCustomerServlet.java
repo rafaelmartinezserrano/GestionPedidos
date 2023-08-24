074478f5cbc7002eb4c2a20b22d9360c7ed52364
Alan <aklt1994@gmail.com>
2023-08-24 11:10:50 +0300

    Finish NewCustomerServlet

--- src/main/java/com/afdm/gestionpedidos/controller/NewCustomerServlet.java
+++ src/main/java/com/afdm/gestionpedidos/controller/NewCustomerServlet.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.afdm.gestionpedidos.model.Customer;
+import com.afdm.gestionpedidos.model.facade.GestionPedidosFacade;
+
 /**
  * Servlet implementation class NewCustomerServlet
  */
@@ -17,7 +20,9 @@ public class NewCustomerServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String customerID = request.getParameter("customerID");
+		
+		boolean signedUp = false;
+		
 		String companyName = request.getParameter("companyName");
 		String contactName = request.getParameter("contactName");
 		String contactTitle = request.getParameter("contactTitle");
@@ -26,8 +31,33 @@ public class NewCustomerServlet extends HttpServlet {
 		String region = request.getParameter("region");
 		String postalCode = request.getParameter("postalCode");
 		String country = request.getParameter("country");
-		String phone = ;
-		String fax;
+		String phone = request.getParameter("phone");
+		String fax = request.getParameter("fax");
+		
+		Customer customer = new Customer(companyName, contactName, contactTitle, address, city, region, postalCode, country, phone, fax);
+		
+		try {
+			
+			GestionPedidosFacade facade = new GestionPedidosFacade();
+			
+			signedUp = facade.signUpCustomer(customer);
+			
+			if (signedUp) {
+				
+				request.setAttribute("Register", signedUp);
+				
+			} else {
+				
+				request.setAttribute("error", "No se ha podido registrar al cliente");
+				
+			}
+			
+		} catch(Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+		
 	}
 
 }
